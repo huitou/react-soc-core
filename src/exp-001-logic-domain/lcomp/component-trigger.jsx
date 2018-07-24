287e2b7c183aeb37f719877a0bb0081357dc59eb
huitou <3.riverside.swe@gmail.com>
2018-07-24 18:56:59 +0100

    Code refinement

--- src/exp-001-logic-domain/lcomp/component-trigger.jsx
+++ src/exp-001-logic-domain/lcomp/component-trigger.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { ITrigger } from './interface-trigger';
@@ -77,8 +77,6 @@ export default class LTrigger extends Component {
     }
 
     render() {
-        return (
-            <div>LTrigger</div>
-        );
+        return null;
     }
 }
