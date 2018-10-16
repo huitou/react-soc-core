fddafd0c2a585541efa8f5eaf6ea83bac6ad37e0
huitou <3.riverside.swe@gmail.com>
2018-10-16 12:22:26 +0100

    Prettiering

--- src/core/withLInterface.js
+++ src/core/withLInterface.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
-export const withLInterface = (ChildComponent, LInterface) => (hefc, role) => (props) => {
-    const interface = new LInterface(hefc, role);
-    const ref = React.createRef();
+export const withLInterface = (ChildComponent, LInterface) => (
+  hefc,
+  role
+) => props => {
+  const interface = new LInterface(hefc, role);
+  const ref = React.createRef();
 
-    interface._ref = ref;
-    role.interface = interface   // This is a cyclic reference.
+  interface._ref = ref;
+  role.interface = interface; // This is a cyclic reference.
 
-    return (<ChildComponent ref={ref} interface={interface} {...props} />)
+  return <ChildComponent ref={ref} interface={interface} {...props} />;
 };
