a2fc950141e974fa5ffd7574fd1997bb4e8c633d
huitou <3.riverside.swe@gmail.com>
2018-10-19 22:15:21 +0100

    Refining and adding behaviour tests

--- src/core/core.behaviour.test.js
+++ src/core/core.behaviour.test.js
@@ -4,7 +4,13 @@ import { shallow, mount } from "enzyme";
 import LInterface from "./LInterface";
 import { withLInterface } from "./withLInterface";
 
+const HEFP = { hefp1: jest.fn() };
+const HIFP = { hifp1: jest.fn() };
+
 class Placeholder extends Component {
+  hefp = HEFP;
+  hifp = HIFP;
+
   render() {
     return <div />;
   }
@@ -18,12 +24,12 @@ describe("Core", () => {
       InterfacedComponent = withLInterface(Placeholder, LInterface)();
     });
 
-    it("mounts simply the component ignoring the interface", () => {
+    it("mounts a component with interface without getting interface's reference", () => {
       const wrapper = shallow(<InterfacedComponent />);
       expect(wrapper.find(Placeholder).length).toEqual(1);
     });
 
-    it("mounts simply the component ignoring the interface", () => {
+    it("mounts a component with interface without getting interface's reference", () => {
       const wrapper = mount(<InterfacedComponent />);
       expect(wrapper.find(Placeholder).length).toEqual(1);
       expect(wrapper.find("div").length).toEqual(1);
@@ -36,22 +42,34 @@ describe("Core", () => {
       InterfacedComponentFunc = withLInterface(Placeholder, LInterface);
     });
 
-    it("mounts the component and set the interface instance", () => {
+    it("mounts a component with interface and gets interface's reference", () => {
       const role = { name: 'test' };
       const InterfacedComponent = InterfacedComponentFunc(undefined, role)
       const wrapper = shallow(<InterfacedComponent />);
-      expect(wrapper.find(Placeholder).length).toEqual(1);
       expect(role.interface instanceof LInterface).toEqual(true);
     });
 
-    it("mounts the component and set the interface instance", () => {
+    it("mounts a component with interface's hefp and hifp set", () => {
       const role = { name: 'test' };
       const InterfacedComponent = InterfacedComponentFunc(undefined, role)
       const wrapper = mount(<InterfacedComponent />);
-      expect(wrapper.find(Placeholder).length).toEqual(1);
-      expect(wrapper.find("div").length).toEqual(1);
-      expect(role.interface instanceof LInterface).toEqual(true);
-      console.log(role.interface);
+      expect(role.interface.hefp()).toEqual(HEFP);
+      expect(role.interface.hifp()).toEqual(HIFP);
+    });
+  });
+
+  describe("when used with parameters hefc and role", () => {
+    let InterfacedComponentFunc;
+    beforeEach(() => {
+      InterfacedComponentFunc = withLInterface(Placeholder, LInterface);
+    });
+
+    it("mounts a component with interface's hefc set", () => {
+      const hefc = { test: jest.fn() }
+      const role = { name: 'test' };
+      const InterfacedComponent = InterfacedComponentFunc(hefc, role)
+      const wrapper = shallow(<InterfacedComponent />);
+      expect(role.interface.hefc()).toEqual(hefc);
     });
   });
 
