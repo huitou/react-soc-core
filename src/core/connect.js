8c55514ea6fe621a3ba113543d9ef07b3e7a404b
huitou <3.riverside.swe@gmail.com>
2018-12-25 22:52:14 +0000

    Fixing connector error

--- src/core/connect.js
+++ src/core/connect.js
@@ -3,42 +3,46 @@
   and to inject values and handles provided by the model.
 
   Copyright (c) 2018 Riverside Software Engineering Ltd. All rights reserved.
-  Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+  Licensed under the MIT License.
+  See LICENSE file in the project root for full license information.
 */
 
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const connect = (LModel, name) => (WrappedComponent) => {
-    const root = {
-        collector: undefined,
-        ref: React.createRef(),
-    };
-
-    const changeEventHandle = () => {
-        root.ref.current && root.ref.current.forceUpdate();
-    };
-
-    const register = (collectorInstance) => {
-        root.collector = collectorInstance;
-        return changeEventHandle;
-    };
-
-    const ldConfig = { name, register };
-
+export const connect = (Model, name) => (WrappedComponent) => {
     class Wrapper extends React.Component {
         render() {
-            return root.collector
-                ? <WrappedComponent {...this.props} {...root.collector.valueAndHandleTree()} />
+            const collector = this.props.getCollector();
+            return collector
+                ? <WrappedComponent {...this.props} {...collector.valueAndHandleTree()} />
                 : null;
         }
     }
 
     return (props) => {
+        const root = {
+            collector: undefined,
+            ref: React.createRef(),
+        };
+
+        const changeEventHandle = () => {
+            root.ref.current && root.ref.current.forceUpdate();
+        };
+
+        const register = (collectorInstance) => {
+            root.collector = collectorInstance;
+            return changeEventHandle;
+        };
+
+        const ldConfig = { name, register };
+        const getCollector = () => root.collector;
+
         return (
             <React.Fragment>
-                <Wrapper {...props} ref={root.ref} />
-                <LModel {...props} ldConfig={ldConfig} />
+                <Wrapper {...props} ref={root.ref} getCollector={getCollector} />
+                <Model {...props} ldConfig={ldConfig} />
             </React.Fragment>
         );
     };
