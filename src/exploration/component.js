b65540668af35a3b18dd41005453d59fd41703fd
huitou <3.riverside.swe@gmail.com>
2019-07-28 23:01:02 +0100

    changing repository to testing purposes

---
+++ src/exploration/component.js
@@ -0,0 +1,45 @@
+/*
+    Object Model.
+
+    Copyright (c) 2019 Riverside Software Engineering Ltd. All rights reserved.
+
+    Licensed under the MIT License. See LICENSE file in the project root for full license information.
+*/
+
+import React, { Component } from 'react';
+import { object } from 'prop-types';
+import { mergeDeepRight } from 'ramda';
+
+class ObjectModel extends Component {
+    static propTypes = {
+        initial: object
+    };
+
+    static defaultProps = {
+        initial: {}
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            initial: props.initial,
+            value: props.initial,
+        };
+    }
+
+    value = () => this.state.value;
+    
+    change = (obj) => {
+        this.setState(({ value }) => ({ value: mergeDeepRight(value, obj) }));
+    };
+
+    reset = () => {
+        this.setState(({ initial }) => ({ value: initial }));
+    };
+
+    render() {
+        return null;
+    }
+}
+
+export default ObjectModel;
